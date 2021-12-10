f92e2b6c1970362b90e5239526f1d6374a430f01
Pieter van der Heijden <pietervdheijden@gmail.com>
2021-12-10 23:28:33 +0100

    Added solution for 2021 day 4 assignment 2

--- 2021/day4/index.js
+++ 2021/day4/index.js
@@ -3,18 +3,19 @@ const fs = require('fs')
 function getDataset()
 {
   const data = fs.readFileSync('dataset.txt', 'utf8');
+  // const data = fs.readFileSync('dataset-example.txt', 'utf8');
   
   var drawnNumbers = [];
   var boards = [];
   data.split(/\r?\n\n/).forEach(function(block) {
     if (block.includes(',')) {
-      drawnNumbers = block.split(',');
+      drawnNumbers = block.split(',').map(n => parseInt(n));
     } else {
       var board = [];
       block.split(/\r?\n/).forEach(function(line) {
         line = line.trim();
         line = line.replaceAll('  ', ' ')
-        var row = line.split(' ');
+        var row = line.split(' ').map(n => parseInt(n));
         board.push(row);
       });
       boards.push(board);
@@ -27,12 +28,53 @@ function getDataset()
   };
 }
 
+// get first bingos
 function assignment1() {
   var dataset = getDataset();
 
   var drawnNumbers = dataset.drawnNumbers;
   var boards = dataset.boards;
 
+  var bingos = play(drawnNumbers, boards);
+  if (!bingos) {
+    throw 'No bingo could be found!'
+  }
+  
+  console.log('\nAssignment 1:');
+  console.log('- First bingos: ');
+  bingos.forEach(bingo => { 
+    console.log(bingo)
+  });
+}
+
+// get last bingos
+function assignment2() {
+  var dataset = getDataset();
+
+  var drawnNumbers = dataset.drawnNumbers;
+  var boards = dataset.boards;
+
+  var bingos = []
+  while (boards.length > 0 && drawnNumbers.length != 0) {
+    bingos = play(drawnNumbers, boards);
+    if (!bingos) {
+      throw 'No bingo could be found!'
+    }
+
+    drawnNumbers = drawnNumbers.slice(bingos[0].drawnNumberIndex+1, drawnNumbers.length);
+    bingos.forEach(bingo => {
+      boards = boards.filter(b => b != bingo.board)
+    });
+  }
+
+  console.log('\nAssignment 2:');
+  console.log('- Last bingos: ');
+  bingos.forEach(bingo => {
+    console.log(bingo);
+  })
+}
+
+function play(drawnNumbers, boards) {
   for (var drawnNumberIndex = 0; drawnNumberIndex < drawnNumbers.length; drawnNumberIndex++) {
     var drawnNumber = drawnNumbers[drawnNumberIndex];
     
@@ -48,31 +90,17 @@ function assignment1() {
     });
 
     // Check if any of the boards has bingo
+    var bingos = [];
+    outerLoop:
     for (var boardIndex = 0; boardIndex < boards.length; boardIndex++) {
       var board = boards[boardIndex];
-      console.log(board);
+      
       // Check if row is complete
       for (var rowIndex = 0; rowIndex < board.length; rowIndex++) {
         var row = board[rowIndex];
         if (row.filter((value) => value != -1).length == 0) {
-          console.log('Bingo!');
-          console.log(board);
-          console.log(`drawnNumber: ${drawnNumber}`)
-
-          var sumOfUnmarkedNumbers = 0;
-          board.forEach((row) => {
-            row.forEach((value) => {
-              if (value != -1) {
-                sumOfUnmarkedNumbers += parseInt(value);
-              }
-            })
-          })
-          console.log(`sumOfUnmarkedNumbers: ${sumOfUnmarkedNumbers}`)
-
-          var finalScore = sumOfUnmarkedNumbers * drawnNumber;
-          console.log(`finalScore: ${finalScore}`)
-
-          return;
+          bingos.push(getBingo(drawnNumberIndex, drawnNumber, boardIndex, board));
+          continue outerLoop;
         }
       }
 
@@ -80,30 +108,40 @@ function assignment1() {
       for (var columnIndex = 0; columnIndex < board.length; columnIndex++) {
         var column = [];
         for (var rowIndex = 0; rowIndex < board.length; rowIndex++) {
-          column.push(board[columnIndex][rowIndex]);
+          column.push(board[rowIndex][columnIndex]);
         }
-
         if (column.filter((value) => value != -1).length == 0) {
-          console.log('Bingo!');
-          console.log(board);
-          console.log(`drawnNumber: ${drawnNumber}`)
-
-          var sumOfUnmarkedNumbers = 0;
-          board.forEach((row) => {
-            row.forEach((value) => {
-              if (value != -1) {
-                sumOfUnmarkedNumbers += parseInt(value);
-              }
-            })
-          })
-          console.log(`sumOfUnmarkedNumbers: ${sumOfUnmarkedNumbers}`)
-
-          var finalScore = sumOfUnmarkedNumbers * drawnNumber;
-          console.log(`finalScore: ${finalScore}`)
+          bingos.push(getBingo(drawnNumberIndex, drawnNumber, boardIndex, board));
+          continue outerLoop;
         }
       }
+    }
+    if (bingos.length > 0) {
+      return bingos;
     }
   }
 }
 
-assignment1()
+function getBingo(drawnNumberIndex, drawnNumber, boardIndex, board) {
+  var sumOfUnmarkedNumbers = 0;
+  board.forEach((row) => {
+    row.forEach((value) => {
+      if (value != -1) {
+        sumOfUnmarkedNumbers += value;
+      }
+    })
+  })
+  var finalScore = sumOfUnmarkedNumbers * drawnNumber;
+
+  return {
+    drawnNumberIndex: drawnNumberIndex,
+    drawnNumber: drawnNumber,
+    boardIndex: boardIndex,
+    board: board,
+    sumOfUnmarkedNumbers: sumOfUnmarkedNumbers,
+    finalScore
+  }
+}
+
+assignment1();
+assignment2();
